a9c994b9780d95edd0fd671203fcd2c84d73ba2e
toannv2000 <66178633+toannv2000@users.noreply.github.com>
2021-11-17 12:02:55 +0700

    Done Recipe Detail Design

---
+++ app/src/main/java/com/example/foody/fragment/OverviewFragment.java
@@ -0,0 +1,41 @@
+package com.example.foody.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foody.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link OverviewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OverviewFragment extends Fragment {
+
+
+
+    public OverviewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_overview, container, false);
+    }
+}
